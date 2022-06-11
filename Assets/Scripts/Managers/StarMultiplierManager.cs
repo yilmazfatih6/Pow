88be71f28f2c2ddc065eb4ca92af2dac1716694e
Fatih Yılmaz <yilmazfatih6@outlook.com>
2022-06-12 02:33:11 +0300

    Star multiplier manager ve ui eklendi.

---
+++ Assets/Scripts/Managers/StarMultiplierManager.cs
@@ -0,0 +1,51 @@
+﻿using DG.Tweening;
+using ScriptableObjectArchitecture;
+using UnityEngine;
+
+namespace Managers
+{
+    [CreateAssetMenu(fileName = "StarMultiplierManager", menuName = "ScriptableObject/StarMultiplierManager", order = 0)]
+    public class StarMultiplierManager : ScriptableObject
+    {
+        [SerializeField] private float duration = 7;
+        [SerializeField] private FloatVariable timerDurationPerfect;
+        [SerializeField] private IntVariable starMultiplier;
+        [SerializeField] private GameEvent onMerge;
+        private Tween _tween;
+        
+        private void OnEnable()
+        {
+            onMerge.AddListener(IncreaseMultiplier);
+            starMultiplier.Value = 1;
+        }
+
+        private void OnDisable()
+        {
+            onMerge.RemoveListener(IncreaseMultiplier);
+        }
+        
+        private void IncreaseMultiplier()
+        {
+            starMultiplier.Value++;
+            ResetTimer();
+        }
+        
+        private void ResetMultiplier()
+        {
+            starMultiplier.Value = 1;
+            ResetTimer();
+        }
+
+        private void ResetTimer()
+        {
+            _tween?.Kill();
+
+            if (starMultiplier.Value == 1) return;
+            
+            _tween = DOVirtual.Float(1, 0, duration, (x) =>
+            {
+                timerDurationPerfect.Value = x;
+            }).OnComplete(ResetMultiplier);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MultiplierBarUI.cs
@@ -0,0 +1,40 @@
+﻿using ScriptableObjectArchitecture;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MultiplierBarUI : MonoBehaviour
+    {
+        [Header("SO Data")]
+        [SerializeField] private FloatVariable timerDurationPerfect;
+        [SerializeField] private IntVariable starMultiplier;
+
+        [Header("UI References")]
+        [SerializeField] private Image processBar;
+        [SerializeField] private TextMeshProUGUI multiplierText;
+        
+        private void OnEnable()
+        {
+            timerDurationPerfect.AddListener(OnTimerDurationPerfectChange);
+            starMultiplier.AddListener(OnStarMultiplierChange);
+        }
+
+        private void OnDisable()
+        {
+            timerDurationPerfect.RemoveListener(OnTimerDurationPerfectChange);
+            starMultiplier.RemoveListener(OnStarMultiplierChange);
+        }
+
+        private void OnTimerDurationPerfectChange(float value)
+        {
+            processBar.fillAmount = value;
+        }
+
+        private void OnStarMultiplierChange(int value)
+        {
+            multiplierText.text = "X" + value;
+        }
+    }
+}
