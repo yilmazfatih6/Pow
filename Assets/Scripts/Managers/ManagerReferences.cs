7903330d8cd8b2cfb4e4fb865bee9ce4b5681a2f
Fatih Yılmaz <yilmazfatih6@outlook.com>
2022-06-12 23:34:11 +0300

    Manager References eklendi.

---
+++ Assets/Scripts/Managers/ManagerReferences.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Managers
+{
+    public class ManagerReferences : MonoBehaviour
+    {
+        [SerializeField] private List<ScriptableObject> managers;
+    }
+}
